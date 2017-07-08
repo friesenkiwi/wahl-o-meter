92d9ce77b86933da8822a4c4d27ad246c8fb81c8
friesenkiwi <friesenkiwi@denk-nach-mcfly.de>
2017-07-08 14:50:49 +0200

    Work on theses and show them on page

--- converter.js
+++ converter.js
@@ -12,3 +12,45 @@ function wahlomat_collect(year, parliament){
   WOMT_aParteienURL=undefined;
   WOMT_aTexte=undefined;
 }
+
+function wahlomat_convert_theses(allData){
+  var convertedData=[];
+  var curWOM;
+  var currentThesis={}; //converted
+  var curThe; //original
+  for (var i = 0; i < allData.length; i++) {
+    curWOM=allData[i];
+    var theses=[];
+
+    for(var j = 0; j < curWOM.theses.theses.length; j++) {
+      curThe=curWOM.theses.theses[j][0];
+    //  console.log(curThe);
+      if(curThe[0]==curThe[1]){
+        curThe[0]="";
+      }
+      currentThesis={"title":curThe[0], "description":curThe[1]};
+
+      theses.push(currentThesis);
+    }
+    curWOM.theses=theses;
+
+    convertedData.push(curWOM);
+  }
+  return convertedData;
+}
+
+function wahlomat_dump_theses(allData){
+  for (var i = 0; i < allData.length; i++) {
+    var title="";
+    if(allData[i].wahlomat.texts["wahlomat_titelzeile"]!=undefined){
+      title=allData[i].wahlomat.texts["wahlomat_titelzeile"][0];
+    }else if (allData[i].wahlomat.texts["wahlomat_head_titel"]!=undefined) {
+      title=allData[i].wahlomat.texts["wahlomat_head_titel"][0];
+    }
+    document.write("<h2>"+allData[i].wahlomat.id+" - "+title+"</h2>");
+    for(var j = 0; j < allData[i].theses.length; j++) {
+      var curThe=allData[i].theses[j];
+      document.write(curThe.title +": "+curThe.description+"<br />");
+    }
+  }
+}
